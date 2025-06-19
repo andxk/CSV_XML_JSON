5fb2927c0759625ccc58269b0d69b1243f8b430c
andxk <kupex@ya.ru>
2025-06-19 18:37:31 +0300

    Add XMLtoJSON (HW2)

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,10 +1,8 @@
 package ru.netology;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -15,6 +13,12 @@ import com.opencsv.CSVReader;
 import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 
 public class Main {
@@ -37,6 +41,47 @@ public class Main {
             return employees;
 
         } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+
+    public static List<Employee> parseXML(String fileName) {
+
+        try {
+            DocumentBuilderFactory documentFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder documentBuilder = documentFactory.newDocumentBuilder();
+            Document doc = documentBuilder.parse(new File(fileName));
+
+//            Node root = doc.getDocumentElement();
+//            System.out.println( "Корневой элемент: " + root.getNodeName());
+
+            List<Employee> list = new ArrayList<>();
+            NodeList nodeList = doc.getElementsByTagName("employee");
+
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                NodeList empFields = nodeList.item(i).getChildNodes();
+                Employee employee = new Employee();
+                for (int j = 0; j < empFields.getLength(); j++) {
+                    Node field = empFields.item(j);
+                    if (Node.ELEMENT_NODE == field.getNodeType()) {
+                        System.out.println(field.getNodeName() +" " + field.getTextContent());
+                        switch (field.getNodeName()) {
+                            case "id" : employee.id = Integer.parseInt(field.getTextContent()); break;
+                            case "age" : employee.age = Integer.parseInt(field.getTextContent()); break;
+                            case "firstName" : employee.firstName = field.getTextContent(); break;
+                            case "lastName" : employee.lastName = field.getTextContent(); break;
+                            case "country" : employee.country = field.getTextContent(); break;
+                        }
+                    }
+                }
+                list.add(employee);
+            }
+            return list;
+
+        } catch (ParserConfigurationException | IOException | SAXException e) {
+//            throw new RuntimeException(e);
             System.out.println(e.getMessage());
         }
         return null;
@@ -70,18 +115,20 @@ public class Main {
 
 
     public static void main(String[] args) {
-//        System.out.println("Hello world!");
-
         String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
 
-        String fileName = "data.csv";
-        String jsonName = "data.json";
+        final String csvFileName = "data.csv";
+        final String jsonFromCsvName = "data.json";
+        final String xmlFileName = "data.xml";
+        final String jsonFromXmlName = "data2.json";
 
-        List<Employee> employees = parseCSV(columnMapping, fileName);
-
+        List<Employee> employees = parseCSV(columnMapping, csvFileName);
         String json = listToJson(employees);
+        writeString(json, jsonFromCsvName);
 
-        writeString(json, jsonName);
+        employees = parseXML(xmlFileName);
+        json = listToJson(employees);
+        writeString(json, jsonFromXmlName);
 
     }
 }
